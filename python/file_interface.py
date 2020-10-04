c472971545324790a80e08efe0fb80d7d62d6daf
JohnFarmer96 <jonathan.bauer96@googlemail.com>
2020-10-04 22:02:43 +0200

    Update Jupyter Nodebook

--- python/file_interface.py
+++ python/file_interface.py
@@ -36,3 +36,26 @@ def load_specific(index):
 def write_array(array, index): 
     filename = basename_write + str(index) + '.dat'
     np.savetxt(filename, array, fmt='%18.12f')
+
+# Write Config.d file
+def write_config(directory, concentration=10, t_total=1, t_res=0.1, v_x=0, v_y=0, v_z=0, vw_x=0, vw_y=0, vw_z=0, humid=0.5, T_env=300):
+    if(os.path.isfile(directory) == True):
+        output = str(concentration) + " \t :: concentration [1] \n" 
+        output = output + str(t_total) + "\t :: t_total [s] \n" 
+        output = output + str(t_res) + "\t :: t_resolution [s] \n" 
+        output = output + str(v_x) + "\t :: v_x [m/s] \n" 
+        output = output + str(v_y) + "\t :: v_y [m/s] \n"
+        output = output  + str(v_z) + "\t :: v_z [m/s] \n"
+        output = output  + str(vw_x) + "\t :: v_wind_x [m/s] \n"
+        output = output  + str(vw_y) + "\t :: v_wind_y [m/s] \n"
+        output = output  + str(vw_z) + "\t :: v_wind_z [m/s] \n"
+        output = output  + str(humid) + "\t :: humidity [% 0..1] \n"
+        output = output + str(T_env) + "\t :: T_environment [K] \n"
+
+        with open(directory, "w") as text_file:
+            print(output, file=text_file)
+
+        print("Config written successfully")
+    else: 
+        print("Directory '" + str(directory) + "' was not found")
+
